77d7d236609ed85e46528d4eebac8a60bd58034b
Maartje Eyskens <meyskens@me.com>
2017-05-01 16:43:45 +0200

    add env variables for config

--- dispatchd/config/config.go
+++ dispatchd/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"encoding/json"
 	"os"
+	"runtime"
+	"strconv"
 )
 
 // ConfigurationInfo contains the config file's content
@@ -24,6 +26,7 @@ func newConfigurationInfo() ConfigurationInfo {
 	config.BindPort = 7384 // "IT" in ASCII to decimal
 	config.EtcdAddress = "http://127.0.0.1:2379"
 	config.Zone = "dc"
+	config.Arch = runtime.GOARCH
 	return config
 }
 
@@ -31,13 +34,33 @@ func newConfigurationInfo() ConfigurationInfo {
 func GetConfiguration() ConfigurationInfo {
 	returnConfig := newConfigurationInfo()
 	data, err := os.Open("config.json")
-	if err != nil {
-		panic(err)
+	if err == nil { // Only read when available
+		jsonParser := json.NewDecoder(data)
+		jsonParser.Decode(&returnConfig)
 	}
-	jsonParser := json.NewDecoder(data)
-	err = jsonParser.Decode(&returnConfig)
-	if err != nil {
-		panic(err)
-	}
+	readEnv(&returnConfig)
 	return returnConfig
+}
+
+func readEnv(conf *ConfigurationInfo) {
+	if name := os.Getenv("DISPATCH_MACHINENAME"); name != "" {
+		conf.MachineName = name
+	}
+	if bindip := os.Getenv("DISPATCH_BINDIP"); bindip != "" {
+		conf.BindIP = bindip
+	}
+	if bindport := os.Getenv("DISPATCH_BINDPORT"); bindport != "" {
+		if port, err := strconv.Atoi(bindport); err == nil {
+			conf.BindPort = port
+		}
+	}
+	if etcdaddress := os.Getenv("DISPATCH_ETCDADDRESS"); etcdaddress != "" {
+		conf.EtcdAddress = etcdaddress
+	}
+	if publicip := os.Getenv("DISPATCH_PUBLICIP"); publicip != "" {
+		conf.PublicIP = publicip
+	}
+	if zone := os.Getenv("DISPATCH_ZONE"); zone != "" {
+		conf.Zone = zone
+	}
 }
