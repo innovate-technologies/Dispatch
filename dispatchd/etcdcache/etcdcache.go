8556904f63692b974030645efe158c1430b4e773
Maartje Eyskens <meyskens@me.com>
2017-11-14 19:22:01 +0100

    add etcdcache

---
+++ dispatchd/etcdcache/etcdcache.go
@@ -0,0 +1,58 @@
+package etcdcache
+
+import (
+	"context"
+	"errors"
+	"sync"
+
+	etcd "github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
+	"github.com/innovate-technologies/Dispatch/dispatchd/etcdclient"
+)
+
+var etcdAPI = etcdclient.GetEtcdv3()
+
+// EtcdCache stores etcd records in memory to do requests on
+type EtcdCache struct {
+	cache      map[string][]byte
+	cacheMutex sync.Mutex
+}
+
+// New gives a new empty EtcdCache
+func New() *EtcdCache {
+	return &EtcdCache{
+		cache:      map[string][]byte{},
+		cacheMutex: sync.Mutex{},
+	}
+}
+
+// NewForPrefix  returns a new EtcdCache prefilled with the keys of a specific prefix
+func NewForPrefix(prefix string) (*EtcdCache, error) {
+	cache := New()
+	res, err := etcdAPI.Get(context.Background(), prefix, etcd.WithPrefix())
+	if err != nil {
+		return nil, err
+	}
+	for _, kv := range res.Kvs {
+		cache.Put(kv)
+	}
+
+	return cache, nil
+}
+
+// Get gets a key out of the cache
+func (e *EtcdCache) Get(key string) ([]byte, error) {
+	e.cacheMutex.Lock()
+	defer e.cacheMutex.Unlock()
+	if val, ok := e.cache[key]; ok {
+		return val, nil
+	}
+	return nil, errors.New("Key not in cache")
+}
+
+// Put allows to add a new record
+func (e *EtcdCache) Put(val *mvccpb.KeyValue) {
+	e.cacheMutex.Lock()
+	e.cache[string(val.Key)] = val.Value
+	e.cacheMutex.Unlock()
+}
